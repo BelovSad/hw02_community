d91911e2422859d779bad224bd71b3732718de15
Belov Nikita <belovngkzn@gmail.com>
2022-05-06 01:26:57 +0300

    1st attempt

---
+++ yatube/posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'ну это главная'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = 'список групп'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
